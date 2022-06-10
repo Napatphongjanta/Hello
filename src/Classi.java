a71f9a50afea798ec676e73fd783b1c740188882
Napatphongjanta <voop45622@hotmail.com>
2022-06-09 21:42:19 -0700

    first commit

---
+++ src/Classi.java
@@ -0,0 +1,13 @@
+public class Classi {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int sum;
+        a =5;
+        b = 10;
+        sum = a+b;
+        System.out.println("Hello Work!!!");
+        System.out.println("100+5");
+        System.out.println(sum);
+    }
+}
